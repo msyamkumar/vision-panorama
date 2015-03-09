d209d7462ac0ebd91918c27ea30ba47eeaf28b9e
msyamkum <meenakshi.psgit@gmail.com>
2015-03-09 16:51:44 -0500

    Final cylindrical conversion

--- warpToCylindrical.m
+++ warpToCylindrical.m
@@ -1,28 +1,40 @@
-function [ output_args ] = warpToCylindrical( image, f )
+function [ cylindricalImg ] = warpToCylindrical( image, f )
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%warpToCylindrical: Shift all images on pixelVals by the calculated shifts
+%warpToCylindrical: Computes the inverse map to warp the image 
+%into the cylindrical coordinates
 %   Argument: 
-%       shifts - shifts for individual images with respect to reference
-%       image
-%       pixelVals - pixel values for the original images
+%       image - given input image pixels
+%       f - focal length estimate
 %   Return value:
-%       shiftedPixelVals - shifted image pixels
+%       cylindricalImg - inverse map to cylindrical coordinates
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 rowSize = size(image, 1);
 colSize = size(image, 2);
 
+%Center values
 xc = colSize / 2;
 yc = rowSize / 2;
 
+cylindricalImg = zeros(rowSize, colSize, 3);
+
 for row = 1:rowSize
     for col = 1:colSize
+        %Calculating theta and h
         theta = atan((col - xc) / f);
         h = (row - yc) / sqrt((col - xc)^2 + f^2);
         
+        %Calculating cylindrical image co-ordinates
+        xcap = round(f * theta + xc);
+        ycap = round(f * h + yc);
+        
+        cylindricalImg(ycap, xcap, :) = image(row, col, :);
+        
     end
 end
+
+cylindricalImg = uint8(cylindricalImg);
 
 end
 
