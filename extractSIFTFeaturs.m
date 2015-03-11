f663e642f517e5e63a03e902f85ba395b6cc7736
msyamkum <meenakshi.psgit@gmail.com>
2015-03-11 10:55:45 -0500

    Getting started with SIFT extraction

--- extractSIFTFeaturs.m
+++ extractSIFTFeaturs.m
@@ -6,6 +6,9 @@ function [ output_args ] = extractSIFTFeaturs ( image1, image2 )
 grayImg1 = toGrayScale(image1);
 grayImg2 = toGrayScale(image2);
 
+[sift1, d1] = vl_sift(grayImg1);
+[sift2, d2] = vl_sift(grayImg2);
+
 %imwrite(grayImg1, '/Users/cs/Documents/MATLAB/vision_panaroma/gray1.jpg');
 %imwrite(grayImg2, '/Users/cs/Documents/MATLAB/vision_panaroma/gray2.jpg');
 
