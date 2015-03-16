60b479860f017c699494feac39c413d29cd82072
Akshay Sood <sood@cs.wisc.edu>
2015-03-16 09:02:00 -0500

    Stitching implementation

---
+++ blendFeathering.m
@@ -0,0 +1,9 @@
+function [ out ] = blendFeathering( img1, img2, img1t, img2t )
+% Blends two images using feathering blending
+% input: original and transformed images
+
+% FIXME
+out = img1t + img2t;
+
+end
+
--- panorama.m
+++ panorama.m
@@ -1,10 +1,14 @@
 % master script for running panorama implementation
+clear all;
+close all;
+clc;
 
 %% load image metadata
 disp('Loading image list...');
-% dir = strcat('data/1/');
-dir = strcat('/Users/cs/Desktop/CS766/HW2/newPhotosSmallSize/Bridge/');
-%dir = strcat('/Users/cs/Desktop/CS766/HW2/Pictures/Bridge2/');
+dir = strcat('/Users/akshaysood/Box Sync/CS766/Panorama/data/1/');
+% dir = strcat('/Users/akshaysood/Box Sync/CS766/Panorama/Pictures/Latest Pics/CapitolRoofWithRail - small size photos - require different focal length/');
+% dir = strcat('/Users/cs/Desktop/CS766/HW2/newPhotosSmallSize/Bridge/');
+% dir = strcat('/Users/cs/Desktop/CS766/HW2/Pictures/Bridge2/');
 
 imlistfile = strcat(dir, 'image_list.txt');
 image_names = importdata(imlistfile);
@@ -33,9 +37,10 @@ disp('Done.');
 %% warp to cylindrical coordinates
 disp('Warping to cylindrical coordinates...');
 
-% focal length in pixels (SEE how to determine)
+% focal length in pixels
 %f = 660.8799;
 f = 682.05069;
+%f = 595;
 
 % assumes all cropped images (with black pixels truncated) of the same size
 A = warpToCylindrical(images(:,:,:,1),f);
@@ -60,22 +65,42 @@ isLeft = 1;
 for i=1:num_images-1
     img1 = cylindrical_images(:,:,:,i);
     img2 = cylindrical_images(:,:,:,i+1);
-    if isLeft == 1
+    % homography estimation
+    if isLeft
         H{i} = homographyEstimation(img2, img1);
     else
         H{i} = homographyEstimation(img1, img2);
-    end
-    % homography estimation
-   
+    end 
 end
 disp('Done.');
 
 %% stitch/crop into final image
 disp('Stitching/cropping into final image...');
+clear mosaic;
+mosaic = cylindrical_images(:,:,:,1);
+
+for i=1:num_images-1
+    % stitching
+    img1 = mosaic;
+    img2 = cylindrical_images(:,:,:,i+1);
+    h = H{i};
+    % transpose to match MATLAB convention
+    T = maketform('projective', h');
+    [img2t, xdataimg2t, ydataimg2t] = imtransform(img2, T);
+    % xdataimg2t and ydataimg2t : bounds of the transformed img2
+    xdataout=[min(1,xdataimg2t(1)) max(size(img1,2),xdataimg2t(2))];
+    ydataout=[min(1,ydataimg2t(1)) max(size(img1,1),ydataimg2t(2))];
+    % transform both images with the computed xdata and ydata
+    img2t = imtransform(img2,T,'XData',xdataout,'YData',ydataout);
+    img1t = imtransform(img1,maketform('affine',eye(3)), ...
+        'XData',xdataout,'YData',ydataout);
+    % blending
+    mosaic = blendFeathering(img1, img2, img1t, img2t);
+end
 
 %mosaic = feathering(cylindrical_images, H);
-mosaic = CreateStitchedImage(cylindrical_images, H);
+%mosaic = CreateStitchedImage(cylindrical_images, H);
 
-imwrite(mosaic, '/Users/cs/Documents/MATLAB/vision-panorama/panorama.jpg');
+imwrite(mosaic, 'panorama.jpg');
 
 disp('Done.');
